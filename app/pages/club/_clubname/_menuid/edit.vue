2a1876ad141c93c0c5f10e2d0aac02c4dea5c304
妹尾弦 <gengineer.web@gmail.com>
2020-01-26 00:32:55 +0900

    ページ構成下準備

---
+++ app/pages/club/_clubname/_menuid/edit.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+div
+  section(v-if="matchName")
+    h1 FILE: {{fileName}}
+    ul
+      li ROUTE NAME: {{JSON.stringify($route.name)}}
+      li PATH: {{JSON.stringify($route.path)}}
+      li PARAMS: {{JSON.stringify($route.params)}}
+    nuxt-child
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      FILE_NAME: 'club/_clubname/_menuid/edit.vue'
+    }
+  },
+  computed: {
+    matchName () {
+      console.info(this.$route.name)
+      return this.$route.name === 'club-clubname-menuid-edit'
+    }
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+section
+  border: solid 1px red
+  background-color: blue
+  padding: 3px
+</style>
