3bf90ffe7de220d1551b863ff8139490430ddf06
妹尾弦 <gengineer.web@gmail.com>
2020-01-29 23:55:50 +0900

    丸マークの作成

---
+++ app/components/MarkCircle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Circle" :style="setColor" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    color1: {
+      type: String,
+      required: true
+    },
+    color2: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    setColor () {
+      return `background-image: linear-gradient(-90deg, ${this.color2} 50%, ${this.color1} 50%, ${this.color1})`
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.Circle {
+  width: 1rem;
+  height: 1rem;
+  border: 1px solid #DDD;
+  border-radius: 50%;
+}
+</style>
