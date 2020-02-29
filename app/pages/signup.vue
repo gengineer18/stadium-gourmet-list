d03e6cef6a25f768cf847fdc5df5f4990c8a9f31
妹尾弦 <gengineer.web@gmail.com>
2020-02-29 18:42:49 +0900

    新規登録ページ

--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -1,25 +1,78 @@
 <template>
-  <div>
-    <h1>新規ユーザー登録</h1>
-    <b-button icon-left="facebook">
-      Facebook
-    </b-button>
-    <b-button icon-left="google" @click="loginGoogle">
-      Google
-    </b-button>
-  </div>
+  <section>
+    <div class="title-box has-text-centered">
+      <h1 class="title is-4">
+        スタグル名鑑へようこそ！
+      </h1>
+      <h2 class="subtitle is-6">
+        新規登録(無料)して利用を開始しましょう。
+      </h2>
+    </div>
+    <div class="btn-width">
+      <span @click="loginTwitter">
+        <button-twitter
+          usage="登録"
+          class="is-block btn-margin"
+        />
+      </span>
+      <span @click="loginFacebook">
+        <button-facebook
+          usage="登録"
+          class="is-block btn-margin"
+        />
+      </span>
+      <span @click="loginGoogle">
+        <button-google
+          usage="登録"
+          class="is-block btn-margin"
+        />
+      </span>
+    </div>
+    <div class="signup-box">
+      スタグル名鑑 のアカウントを持っている場合は
+      <nuxt-link to="/login">
+        ログイン
+      </nuxt-link>
+      から。
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import ButtonTwitter from '@/components/Button/ButtonTwitter.vue'
+import ButtonFacebook from '@/components/Button/ButtonFacebook.vue'
+import ButtonGoogle from '@/components/Button/ButtonGoogle.vue'
 
 export default Vue.extend({
+  components: {
+    ButtonTwitter,
+    ButtonFacebook,
+    ButtonGoogle
+  },
   methods: {
 
   }
 })
 </script>
 
-<style>
-
+<style lang="scss">
+.title-box {
+  border-bottom: 1px solid #dddddd;
+}
+.title {
+  margin: 1.3rem auto;
+}
+.btn-width {
+  width: 100%;
+  max-width: 360px;
+}
+.btn-margin {
+  margin-top: 1rem;
+}
+.signup-box {
+  border-top: 1px solid #dddddd;
+  margin-top: 1rem;
+  padding-top: 1.3rem;
+}
 </style>
