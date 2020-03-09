38e9429efe6a12d896a7372930f4bdbc0c9be17c
妹尾弦 <gengineer.web@gmail.com>
2020-03-09 23:56:58 +0900

    ローディング

---
+++ app/pages/account/loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    ログイン処理中...
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  layout: 'loading',
+  created () {
+    this.$store.dispatch('user/checkAuth')
+  },
+  async mounted () {
+    if (await this.$store.state.user.isRegistered) {
+      console.log('true')
+    } else {
+      console.log('false')
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
