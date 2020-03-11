aac9e87efedceda6074ef4acaeeff54ab502c153
妹尾弦 <gengineer.web@gmail.com>
2020-03-11 13:47:15 +0900

    ヘッダーから新規登録を削除

--- app/components/Header/PcHeader.vue
+++ app/components/Header/PcHeader.vue
@@ -37,17 +37,6 @@
       <template slot="end">
         <b-navbar-item v-if="!$store.state.user.isAuth" tag="div">
           <b-button
-            type="is-sub"
-            icon-left="account"
-            size="is-small"
-            tag="nuxt-link"
-            to="/account/signup"
-          >
-            <strong>ユーザー登録</strong>
-          </b-button>
-        </b-navbar-item>
-        <b-navbar-item v-if="!$store.state.user.isAuth" tag="div">
-          <b-button
             type="is-white"
             icon-left="login"
             size="is-small"
