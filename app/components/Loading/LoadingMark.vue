e43c8fc52dc68b0c890697daed2e783ca4825483
妹尾弦 <gengineer.web@gmail.com>
2020-03-16 13:20:22 +0900

    ローディングアニメーションの追加

--- app/components/Loading/LoadingMark.vue
+++ app/components/Loading/LoadingMark.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    Now Loading...
+    <vue-simple-spinner
+      size="100"
+      message="NowLoading..."
+      font-size="18"
+      line-bg-color="#ccddcc"
+      line-fg-color="#f6d04d"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
 export default Vue.extend({
 
 })
