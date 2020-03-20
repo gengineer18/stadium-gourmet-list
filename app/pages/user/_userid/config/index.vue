ef52eb22fc5014d78f083a878dfda371fd63a697
妹尾弦 <gengineer.web@gmail.com>
2020-03-20 21:46:34 +0900

    フォーム追加

--- app/pages/user/_userid/config/index.vue
+++ app/pages/user/_userid/config/index.vue
@@ -1,13 +1,23 @@
 <template>
   <section>
     <div>
-      <p>名前: {{ displayName }}</p>
-      <p>メールアドレス: {{ email }}</p>
+      <form-input
+        v-model="displayName"
+        label="名前"
+        :max-length="20"
+        :required="true"
+      />
+      <form-input
+        v-model="email"
+        label="メールアドレス"
+        :max-length="254"
+        :required="true"
+      />
     </div>
     <div>
       <p>アイコンの変更: {{ photoURL }}</p>
     </div>
-    <div>
+    <div class="danger-area">
       <p>退会する</p>
     </div>
   </section>
@@ -16,8 +26,12 @@
 <script lang="ts">
 import Vue from 'vue'
 import firebase from '~/plugins/firebase'
+import FormInput from '@/components/Form/FormInput.vue'
 
 export default Vue.extend({
+  components: {
+    FormInput
+  },
   data () {
     return {
       displayName: '',
