5d5a8508c130b0912c086eb3204bc3839a4908ae
妹尾弦 <gengineer.web@gmail.com>
2020-03-21 17:35:39 +0900

    名前の変更（firebase）

--- app/pages/user/_userid/config/index.vue
+++ app/pages/user/_userid/config/index.vue
@@ -1,24 +1,45 @@
 <template>
   <section>
-    <div>
-      <form-input
-        v-model="displayName"
-        label="名前"
-        :max-length="20"
-        :required="true"
-      />
-      <form-input
-        v-model="email"
-        label="メールアドレス"
-        :max-length="254"
-        :required="true"
-      />
-    </div>
-    <div>
-      <p>アイコンの変更: {{ photoURL }}</p>
-    </div>
-    <div class="danger-area">
-      <p>退会する</p>
+    <h1 class="title is-5">
+      ユーザー情報設定
+    </h1>
+    <loading-mark v-if="loading" />
+    <div v-if="!loading">
+      <div class="box">
+        <h2 class="title is-6">
+          名前を変更する
+        </h2>
+        <form-input
+          v-model="displayName"
+          label="名前"
+          :max-length="20"
+          :required="true"
+        />
+        <b-button v-if="!displayName" type="is-sub" disabled>
+          変更する
+        </b-button>
+        <b-button v-if="displayName" type="is-sub" @click="updateName()">
+          変更する
+        </b-button>
+      </div>
+      <div class="box">
+        <h2 class="title is-6">
+          アイコンを変更する
+        </h2>
+        <img :src="photoURL" class="is-block mb-1">
+        <b-button type="is-sub">
+          アイコンの変更へ進む
+        </b-button>
+      </div>
+      <div class="box box-danger">
+        <h2 class="title is-6">
+          退会する
+        </h2>
+        <p>退会処理を行うと二度と元に戻すことはできません。</p>
+        <b-button type="is-danger">
+          退会処理へ進む
+        </b-button>
+      </div>
     </div>
   </section>
 </template>
@@ -27,16 +48,18 @@
 import Vue from 'vue'
 import firebase from '~/plugins/firebase'
 import FormInput from '@/components/Form/FormInput.vue'
+import LoadingMark from '@/components/Loading/LoadingMark.vue'
 
 export default Vue.extend({
   components: {
-    FormInput
+    FormInput,
+    LoadingMark
   },
   data () {
     return {
       displayName: '',
-      email: '',
-      photoURL: ''
+      photoURL: '',
+      loading: false
     }
   },
   created () {
@@ -46,13 +69,44 @@ export default Vue.extend({
     const user = firebase.auth().currentUser
     if (user != null) {
       this.displayName = user.displayName || ''
-      this.email = user.email || ''
       this.photoURL = user.photoURL || ''
+    }
+  },
+  methods: {
+    updateName () {
+      this.loading = true
+      const user = firebase.auth().currentUser
+      if (user === null) {
+        console.error('ユーザーデータが取得できませんでした')
+        return
+      }
+      user.updateProfile({
+        displayName: this.displayName
+      })
+        .then(() => {
+          const href = window.location.href
+          window.location.href = href
+        })
+        .catch((error) => {
+        // An error happened.
+          console.error(error)
+          this.loading = false
+        })
     }
   }
 })
 </script>
 
 <style lang="scss" scoped>
+@import "~assets/css/buefy.scss";
 
+.box {
+  padding: 2rem;
+  &-danger {
+    border: 1px solid $danger;
+  }
+}
+.mb-1 {
+  margin-bottom: 1rem;
+}
 </style>
