124312ad4f8c04d3a84570c3167c46c4d6517da2
妹尾弦 <gengineer.web@gmail.com>
2020-03-22 18:00:45 +0900

    post変更機能追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
-import * as functions from 'firebase-functions';
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// データベースの参照を作成
+var db = admin.firestore()
+
+exports.updateUserName = functions.firestore
+  .document('users/{userId}/credentials/{credentialId}')
+  .onUpdate((change: any, context: any) => {
+    const afterCredential = change.after.data();
+    const beforeCredential = change.before.data();
+    if (afterCredential.displayName === beforeCredential.displayName) return 0;
+    const postRef = db.collection('posts')
+    postRef.where('user.id', '==', `${afterCredential.userId}`)
+      .get()
+      .then(function (querySnapshot: any) {
+        querySnapshot.forEach(function (doc: any) {
+          console.info('docId', doc.id)
+          postRef.doc(doc.id).set({ user: { name: afterCredential.displayName }}, { merge: true })
+        })
+      })
+      .catch ((error: any) => {
+        console.error('error', error)
+      })
+    return 0
+  });
