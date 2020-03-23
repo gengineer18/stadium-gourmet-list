773b8f20ff5e976bca0e7bf624dc9bc3bb27ab6a
妹尾弦 <gengineer.web@gmail.com>
2020-03-23 21:46:32 +0900

    削除処理の追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,16 +19,41 @@ exports.updateUserName = functions.firestore
       console.info('no change:', afterCredential.userId)
       return 0;
     }
-    let batch = db.batch();
+    let batch = db.batch()
     const snapshots = await postColGroup.where('user.id', '==', `${afterCredential.userId}`).get()
     await snapshots.docs.map((doc: any, index: any) => {
       //500件毎にcommitしてbatchインスタンスを初期化
       if ((index + 1) % 500 === 0) {
-        batch.commit(); //新しいインスタンス
-        batch = db.batch(); //新たにインスタンスを生成。
+        batch.commit() //新しいインスタンス
+        batch = db.batch() //新たにインスタンスを生成。
       }
       console.info('document ID:',doc.id)
       batch.update(doc.ref, { 'user.name': afterCredential.displayName })
+    })
+    // 最終コミット
+    await batch.commit()
+    return 0
+  });
+
+  exports.deleteUser = functions.firestore
+  .document('users/{userId}/credentials/{credentialId}')
+  .onUpdate(async (change: any, context: any) => {
+    const afterCredential = change.after.data();
+    const beforeCredential = change.before.data();
+    if (beforeCredential.isDelete || !afterCredential.isDelete) {
+      console.info('no delete', afterCredential.isDelete)
+      return 0
+    }
+    let batch = db.batch()
+    const snapshots = await postColGroup.where('user.id', '==', `${afterCredential.userId}`).get()
+    await snapshots.docs.map((doc: any, index: any) => {
+      //500件毎にcommitしてbatchインスタンスを初期化
+      if ((index + 1) % 500 === 0) {
+        batch.commit() //新しいインスタンス
+        batch = db.batch() //新たにインスタンスを生成。
+      }
+      console.info('document ID:',doc.id)
+      batch.update(doc.ref, { 'user.isDelete': true })
     })
     // 最終コミット
     await batch.commit()
