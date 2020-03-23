f3bb2710ab5a16671d3a9fd7b16f112e82f801a1
妹尾弦 <gengineer.web@gmail.com>
2020-03-23 13:39:32 +0900

    ユーザー名変更処理の作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,36 +7,30 @@ const db = admin.firestore()
 
 const postRef = db.collection('posts')
 const userRef = db.collection('users')
+const clubRef = db.collection('clubs')
+const postColGroup = db.collectionGroup('posts')
 
 exports.updateUserName = functions.firestore
   .document('users/{userId}/credentials/{credentialId}')
-  .onUpdate((change: any, context: any) => {
+  .onUpdate(async (change: any, context: any) => {
     const afterCredential = change.after.data();
     const beforeCredential = change.before.data();
-    if (afterCredential.displayName === beforeCredential.displayName) return 0;
-    // post
-    postRef.where('user.id', '==', `${afterCredential.userId}`)
-      .get()
-      .then(function (querySnapshot: any) {
-        querySnapshot.forEach(function (doc: any) {
-          console.info('postdocId', doc.id)
-          postRef.doc(doc.id).set({ user: { name: afterCredential.displayName }}, { merge: true })
-        })
-      })
-      .catch ((error: any) => {
-        console.error('error', error)
-      })
-    // user
-    userRef.doc(afterCredential.userId).collection('posts').where('user.id', '==', `${afterCredential.userId}`)
-      .get()
-      .then(function (querySnapshot: any) {
-        querySnapshot.forEach(function (doc: any) {
-          console.info('userdocId', doc.id)
-          userRef.doc(afterCredential.userId).collection('posts').doc(doc.id).set({ user: { name: afterCredential.displayName }}, { merge: true })
-        })
-      })
-      .catch ((error: any) => {
-        console.error('error', error)
-      })
+    if (afterCredential.displayName === beforeCredential.displayName) {
+      console.info('no change:', afterCredential.userId)
+      return 0;
+    }
+    let batch = db.batch();
+    const snapshots = await postColGroup.where('user.id', '==', `${afterCredential.userId}`).get()
+    await snapshots.docs.map((doc: any, index: any) => {
+      //500件毎にcommitしてbatchインスタンスを初期化
+      if ((index + 1) % 500 === 0) {
+        batch.commit(); //新しいインスタンス
+        batch = db.batch(); //新たにインスタンスを生成。
+      }
+      console.info('document ID:',doc.id)
+      batch.update(doc.ref, { 'user.name': afterCredential.displayName })
+    })
+    // 最終コミット
+    await batch.commit()
     return 0
   });
