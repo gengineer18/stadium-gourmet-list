bdd0f86a164aa6b3e37afab29e4feb45cdd0ee59
妹尾弦 <gengineer.web@gmail.com>
2020-03-29 00:20:28 +0900

    storeの修正

--- app/store/club.ts
+++ app/store/club.ts
@@ -5,14 +5,51 @@ import { toastFail } from '~/utils/common'
 
 const clubRef = db.collection('clubs')
 
-export const state = () => {
-  clubs: []
-}
+export const state = () => ({
+  clubs: [],
+  postData: {
+    postId: '',
+    comment: '',
+    gameDate: null,
+    imagePath: '',
+    price: null,
+    shop: '',
+    createdAt: null,
+    updatedAt: null,
+    club: {
+      id: '',
+      name: ''
+    },
+    user: {
+      id: '',
+      name: '',
+      photo: ''
+    },
+    isDeleted: false
+  }
+})
 
 export const getters = {
   getClubs: (state: any) => state.clubs
 }
 
+export const mutations = {
+  postData(state: any, { data }: any) {
+    state.postData.postId = data.postId
+    state.postData.comment = data.comment
+    state.postData.gameDate = data.gameDate
+    state.postData.imagePath = data.imagePath
+    state.postData.price = data.price
+    state.postData.createdAt = data.createdAt
+    state.postData.updatedAt = data.updatedAt
+    state.postData.club.id = data.club.id
+    state.postData.club.name = data.club.name
+    state.postData.user.id = data.user.id
+    state.postData.user.name = data.user.name
+    state.postData.user.photo = data.user.photo
+    state.postData.isDeleted = data.isDeleted || false
+  }
+}
 
 export const actions = {
   init: firestoreAction(({ bindFirestoreRef }, ref) => {
@@ -38,4 +75,11 @@ export const actions = {
         throw error;
       })
   }),
+  initPost: async ({ commit }: any, { docRefId }: any) => {
+    const postData = await db.collectionGroup('posts').where('postId', '==', docRefId).get()
+    await postData.docs.map((doc) => {
+      console.info('document ID:', doc.data())
+      commit('postData', { data: doc.data() })
+    })
+  },
 }
