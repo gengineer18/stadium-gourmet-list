735f2a3a30626d17b5074fdd4a4ea7b82a2e06e7
妹尾弦 <gengineer.web@gmail.com>
2020-03-29 11:04:06 +0900

    不要処理の削除

--- app/store/club.ts
+++ app/store/club.ts
@@ -1,9 +1,5 @@
 import { firestoreAction } from 'vuexfire'
-import firebase from '~/plugins/firebase'
 import { db } from '~/plugins/firebase'
-import { toastFail } from '~/utils/common'
-
-const clubRef = db.collection('clubs')
 
 export const state = () => ({
   clubs: [],
@@ -40,6 +36,7 @@ export const mutations = {
     state.postData.gameDate = data.gameDate
     state.postData.imagePath = data.imagePath
     state.postData.price = data.price
+    state.postData.shop = data.shop
     state.postData.createdAt = data.createdAt
     state.postData.updatedAt = data.updatedAt
     state.postData.club.id = data.club.id
@@ -55,25 +52,6 @@ export const actions = {
   init: firestoreAction(({ bindFirestoreRef }, ref) => {
     // return the promise returned by `bindFirestoreRef`
     return bindFirestoreRef('clubs', ref)
-  }),
-  add: firestoreAction((context, { postData, docId, clubId }) => {
-    postData.createdAt = firebase.firestore.FieldValue.serverTimestamp()
-    postData.updatedAt = firebase.firestore.FieldValue.serverTimestamp()
-    clubRef.doc(clubId).collection('posts').doc(docId).set(postData)
-      .catch((error) => {
-        console.error('Error adding club doc: ', error.code)
-        toastFail('データベースへの登録に失敗しました。')
-        throw error;
-    })
-  }),
-  deletePost: firestoreAction((context, { postId, clubId }) => {
-    const deleteData = { isDeleted: true, updatedAt: firebase.firestore.FieldValue.serverTimestamp() }
-    clubRef.doc(clubId).collection('posts').doc(postId).set(deleteData, { merge: true })
-      .catch((error: any) => {
-        console.error('Error adding user doc: ', error.code)
-        toastFail('データベースの更新に失敗しました。')
-        throw error;
-      })
   }),
   initPost: async ({ commit }: any, { docRefId }: any) => {
     const postData = await db.collectionGroup('posts').where('postId', '==', docRefId).get()
--- app/store/post.ts
+++ app/store/post.ts
@@ -1,9 +1,4 @@
 import { firestoreAction } from 'vuexfire'
-import firebase from '~/plugins/firebase'
-import { db } from '~/plugins/firebase'
-import { toastFail } from '~/utils/common'
-
-const postRef = db.collection('posts')
 
 export const state = () => {
   posts: []
@@ -13,29 +8,9 @@ export const getters = {
   getPosts: (state: any) => state.posts
 }
 
-
 export const actions = {
   init: firestoreAction(({ bindFirestoreRef }, ref) => {
     // return the promise returned by `bindFirestoreRef`
     return bindFirestoreRef('posts', ref)
-  }),
-  add: firestoreAction((context, { postData, docId }) => {
-    postData.createdAt = firebase.firestore.FieldValue.serverTimestamp()
-    postData.updatedAt = firebase.firestore.FieldValue.serverTimestamp()
-    postRef.doc(docId).set(postData)
-      .catch((error) => {
-        console.error('Error adding post doc: ', error.code)
-        toastFail('データベースへの登録に失敗しました。')
-        throw error;
-    })
-  }),
-  deletePost: firestoreAction((context, { postId }) => {
-    const deleteData = { isDeleted: true, updatedAt: firebase.firestore.FieldValue.serverTimestamp() }
-    postRef.doc(postId).set(deleteData, { merge: true })
-      .catch((error: any) => {
-        console.error('Error adding user doc: ', error.code)
-        toastFail('データベースの更新に失敗しました。')
-        throw error;
-      })
-  }),
+  })
 }
