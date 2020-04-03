3f819f2fdd214cb07a690467c5cc2cda10aab1e0
妹尾弦 <gengineer.web@gmail.com>
2020-04-03 22:02:06 +0900

    投稿削除時にカウントを減らす機能を追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -77,3 +77,34 @@ exports.createClubPost = functions
     clubRef.doc(clubId).set({ count: clubPostCount }, { merge: true })
     return 0
   });
+
+exports.deleteClubPost = functions
+  .region('asia-northeast1')
+  .firestore
+  .document('users/{userId}/posts/{postId}')
+  .onUpdate(async (change: any, context: any) => {
+    const afterCredential = change.after.data();
+    const beforeCredential = change.before.data();
+    if (afterCredential.isDeleted === beforeCredential.isDeleted) {
+      console.info('no change:', afterCredential.userId)
+      return 0;
+    }
+    const clubId = afterCredential.club.id
+    const userId = afterCredential.user.id
+    const userSnapshot = await userRef.doc(userId).collection('clubs').doc(clubId).get()
+    const userData = userSnapshot.data()
+    let userPostCount = userData ? userData.count - 1 : 0
+    if ( userPostCount < 0 ) {
+      userPostCount = 0
+    }
+    userRef.doc(userId).collection('clubs').doc(clubId).set({ count: userPostCount }, { merge: true })
+
+    const clubSnapshot = await clubRef.doc(clubId).get()
+    const clubData = clubSnapshot.data()
+    let clubPostCount = clubData ? clubData.count - 1 : 0
+    if ( clubPostCount < 0 ) {
+      clubPostCount = 0
+    }
+    clubRef.doc(clubId).set({ count: clubPostCount }, { merge: true })
+    return 0
+  });
